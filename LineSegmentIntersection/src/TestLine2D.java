5e1fbb11b249db93ca01008f797e8862f847572d
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-05-18 15:31:19 -0300

    Breaking test of equal segments

--- LineSegmentIntersection/src/TestLine2D.java
+++ LineSegmentIntersection/src/TestLine2D.java
@@ -10,4 +10,9 @@ public class TestLine2D {
 		Line2d line = new Line2d(1, 2, 1, 2, 3, 4, 3, 4);
 	}
 
+	@Test
+	public void testEqualSegments() throws EqualPointsException{
+		Line2d line = new Line2d(1, 2, 3, 4, 1, 2, 3, 4);
+		assertTrue(line.equalSegments());
+	}
 }
