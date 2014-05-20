33c736e60f88ea6b17a994828e97c4616d359fe4
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-05-19 23:34:37 -0300

    Passing test of select with more columns

--- SQLStringGenerator/src/SQL.java
+++ SQLStringGenerator/src/SQL.java
@@ -4,8 +4,13 @@ public class SQL {
 	public String select(String table, String[] column) {
 		String command_sql = "select ";
 		
-		for(int i=0; i<column.length; i++)
-			command_sql += column[i];
+		if(column.length == 1)
+			command_sql += column[0];
+		else{
+			for(int i=0; i<column.length-1; i++)
+				command_sql += column[i] + ",";
+			command_sql += column[column.length-1];
+		}
 		
 		command_sql += " from " + table;
 		
