e0f5e7379ff2634b44467cc74728e8ede2556368
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-05-19 23:37:55 -0300

    Adding setUp method in test SQL class

--- SQLStringGenerator/src/TestSQL.java
+++ SQLStringGenerator/src/TestSQL.java
@@ -1,23 +1,28 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TestSQL {
 
+	private SQL sql;
+	
+	@Before
+	public void setUp(){
+		this.sql = new SQL();
+	}
+	
 	@Test
 	public void testSimpleSelect(){
-		SQL sql = new SQL();
-		
 		String[] column = {"name"};
 		assertEquals("select name from animal", sql.select("animal", column));
 	}
 
 	@Test
 	public void testSelectWithMoreColumns(){
-		SQL sql = new SQL();
-		
 		String[] column = {"name", "age", "phylum"};
 		assertEquals("select name,age,phylum from animal", sql.select("animal", column));
 	}
+	
 }
