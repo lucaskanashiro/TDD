93945bd46294a03adf6a6a3d92e9192b9bc92e2e
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-05-21 13:19:05 -0300

    Passing test of create Game with anothers Games

--- Rock-Paper-Scissor/src/Game.java
+++ Rock-Paper-Scissor/src/Game.java
@@ -7,10 +7,15 @@ public class Game {
 	public Game(String[] player1, String[] player2) {
 		this.player1 = player1;
 		this.player2 = player2;
+		this.game1 = null;
+		this.game2 = null;
 	}
 
 	public Game(Game game1, Game game2) {
-		// TODO Auto-generated constructor stub
+		this.game1 = game1;
+		this.game2 = game2;
+		this.player1 = null;
+		this.player2 = null;
 	}
 
 	public String[] getPlayer1() {
@@ -21,14 +26,12 @@ public class Game {
 		return this.player2;
 	}
 
-	public Object getGame1() {
-		// TODO Auto-generated method stub
-		return null;
+	public Game getGame1() {
+		return this.game1;
 	}
 
-	public Object getGame2() {
-		// TODO Auto-generated method stub
-		return null;
+	public Game getGame2() {
+		return this.game2;
 	}
 
 }
