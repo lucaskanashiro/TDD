b875c9401c1f9328cbf89a0114c206b17f8d0a21
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-05-21 15:05:33 -0300

    Refactoring tests of RockPaperScissor

--- Rock-Paper-Scissor/src/TestRockPaperScissor.java
+++ Rock-Paper-Scissor/src/TestRockPaperScissor.java
@@ -1,50 +1,52 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TestRockPaperScissor {
+	
+	private RockPaperScissor game;
+	
+	@Before
+	public void setUp(){
+		this.game = new RockPaperScissor();
+	}
 
 	@Test(expected=NoSuchStrategyError.class)
 	public void testWinnerWithWrongStrategy() throws NoSuchStrategyError {
-		RockPaperScissor game = new RockPaperScissor();
-		
 		String[] player1 = {"Armando", "p"};
 		String[] player2 = {"Dave", "b"};
 		
 		GameObject gameOne = new GameObject(player1);
 		GameObject gameTwo = new GameObject(player2);
 		
-		game.winner(gameOne, gameTwo);
+		this.game.winner(gameOne, gameTwo);
 	}
 
 	@Test
 	public void testWinnerOfOneGame() throws NoSuchStrategyError{
-		RockPaperScissor game = new RockPaperScissor();
-		
 		String[] player1 = {"Armando", "P"};
 		String[] player2 = {"Dave", "S"};
 		
 		GameObject gameOne = new GameObject(player1);
 		GameObject gameTwo = new GameObject(player2);
 		
-		GameObject winner = game.winner(gameOne, gameTwo);
+		GameObject winner = this.game.winner(gameOne, gameTwo);
 		
 		assertEquals("Dave", winner.getPlayer()[0]);
 		assertEquals("S", winner.getPlayer()[1]);
 	}
 	
 	@Test
-	public void testWinnerOfOneGameWithSameStrategy() throws NoSuchStrategyError{
-		RockPaperScissor game = new RockPaperScissor();
-		
+	public void testWinnerOfOneGameWithSameStrategy() throws NoSuchStrategyError{	
 		String[] player1 = {"Armando", "P"};
 		String[] player2 = {"Dave", "P"};
 		
 		GameObject gameOne = new GameObject(player1);
 		GameObject gameTwo = new GameObject(player2);
 		
-		GameObject winner = game.winner(gameOne, gameTwo);
+		GameObject winner = this.game.winner(gameOne, gameTwo);
 		
 		assertEquals("Armando", winner.getPlayer()[0]);
 		assertEquals("P", winner.getPlayer()[1]);
@@ -52,25 +54,9 @@ public class TestRockPaperScissor {
 	
 	@Test
 	public void testSimpleTournament() throws NoSuchStrategyError{
-		RockPaperScissor game = new RockPaperScissor();
+		GameObject gameWith4players = this.createTournamentWith8Players()[0];
 		
-		String[] player1 = {"Armando", "P"};
-		String[] player2 = {"Dave", "S"};
-		
-		GameObject gameOne = new GameObject(player1);
-		GameObject gameTwo = new GameObject(player2);
-		
-		GameObject game1 = new GameObject(gameOne, gameTwo);
-		
-		String[] player3 = {"Richard", "R"};
-		String[] player4 = {"Michael", "S"};
-		
-		GameObject gameThree = new GameObject(player3);
-		GameObject gameFour = new GameObject(player4);
-		
-		GameObject game2 = new GameObject(gameThree, gameFour);
-		
-		GameObject winner = game.tournament_winner(game1, game2);
+		GameObject winner = this.game.tournament_winner(gameWith4players.getGame1(), gameWith4players.getGame2());
 		
 		assertEquals("Richard", winner.getPlayer()[0]);
 		assertEquals("R", winner.getPlayer()[1]);
@@ -78,8 +64,32 @@ public class TestRockPaperScissor {
 	
 	@Test
 	public void testTournamentWith8players() throws NoSuchStrategyError{
-		RockPaperScissor game = new RockPaperScissor();
+		GameObject semiFinal1 = this.createTournamentWith8Players()[0];
+		GameObject semiFinal2 = this.createTournamentWith8Players()[1];
 		
+		GameObject winner = this.game.tournament_winner(semiFinal1, semiFinal2);
+		
+		assertEquals("Richard", winner.getPlayer()[0]);
+		assertEquals("R", winner.getPlayer()[1]);
+	}
+	
+	@Test
+	public void testTournamentWith16players() throws NoSuchStrategyError{
+		GameObject quarterFinal1 = this.createTournamentWith8Players()[0];
+		GameObject quarterFinal2 = this.createTournamentWith8Players()[1];
+		GameObject quarterFinal3 = this.createTournamentWith8Players()[0];
+		GameObject quarterFinal4 = this.createTournamentWith8Players()[1];
+		
+		GameObject semiFinal1 = new GameObject(quarterFinal1, quarterFinal2);
+		GameObject semiFinal2 = new GameObject(quarterFinal3, quarterFinal4);
+		
+		GameObject winner = this.game.tournament_winner(semiFinal1, semiFinal2);
+		
+		assertEquals("Richard", winner.getPlayer()[0]);
+		assertEquals("R", winner.getPlayer()[1]);
+	}
+	
+	private GameObject[] createTournamentWith8Players(){
 		String[] player1 = {"Armando", "P"};
 		String[] player2 = {"Dave", "S"};
 		
@@ -115,91 +125,8 @@ public class TestRockPaperScissor {
 		GameObject semiFinal1 = new GameObject(game1, game2);
 		GameObject semiFinal2 = new GameObject(game3, game4); 
 		
-		GameObject winner = game.tournament_winner(semiFinal1, semiFinal2);
+		GameObject[] result = {semiFinal1, semiFinal2};
 		
-		assertEquals("Richard", winner.getPlayer()[0]);
-		assertEquals("R", winner.getPlayer()[1]);
-	}
-	
-	@Test
-	public void testTournamentWith16players() throws NoSuchStrategyError{
-		RockPaperScissor game = new RockPaperScissor();
-		
-		String[] player1 = {"Armando", "P"};
-		String[] player2 = {"Dave", "S"};
-		
-		GameObject gameOne = new GameObject(player1);
-		GameObject gameTwo = new GameObject(player2);
-		
-		GameObject game1 = new GameObject(gameOne, gameTwo);
-		
-		String[] player3 = {"Richard", "R"};
-		String[] player4 = {"Michael", "S"};
-		
-		GameObject gameThree = new GameObject(player3);
-		GameObject gameFour = new GameObject(player4);
-		
-		GameObject game2 = new GameObject(gameThree, gameFour);
-			
-		String[] player5 = {"Allen", "S"};
-		String[] player6= {"Omer", "P"};
-		
-		GameObject gameFive = new GameObject(player5);
-		GameObject gameSix = new GameObject(player6);
-		
-		GameObject game3 = new GameObject(gameFive, gameSix);
-		
-		String[] player7 = {"David R.", "R"};
-		String[] player8 = {"Richard X.", "P"};
-		
-		GameObject gameSeven = new GameObject(player7);
-		GameObject gameEight = new GameObject(player8);
-		
-		GameObject game4 = new GameObject(gameSeven, gameEight);
-		
-		String[] player9 = {"Lua", "S"};
-		String[] player10 = {"Dani", "P"};
-		
-		GameObject gameNine = new GameObject(player9);
-		GameObject gameTen = new GameObject(player10);
-		
-		GameObject game5 = new GameObject(gameNine, gameTen);
-		
-		String[] player11 = {"Sol", "S"};
-		String[] player12 = {"Sergio", "R"};
-		
-		GameObject gameEleven = new GameObject(player11);
-		GameObject gameTwelve = new GameObject(player12);
-		
-		GameObject game6 = new GameObject(gameEleven, gameTwelve);
-		
-		String[] player13 = {"Alice", "P"};
-		String[] player14 = {"Homer", "S"};
-		
-		GameObject gameThirteen = new GameObject(player13);
-		GameObject gameFourteen = new GameObject(player14);
-		
-		GameObject game7 = new GameObject(gameThirteen, gameFourteen);
-		
-		String[] player15 = {"Ronaldo", "P"};
-		String[] player16 = {"Ricardo", "S"};
-		
-		GameObject gameFifteen = new GameObject(player15);
-		GameObject gameSixteen = new GameObject(player16);
-		
-		GameObject game8 = new GameObject(gameFifteen, gameSixteen);
-		
-		GameObject quarterFinal1 = new GameObject(game1, game2);
-		GameObject quarterFinal2 = new GameObject(game3, game4);
-		GameObject quarterFinal3 = new GameObject(game5, game6);
-		GameObject quarterFinal4 = new GameObject(game7, game8);
-		
-		GameObject semiFinal1 = new GameObject(quarterFinal1, quarterFinal2);
-		GameObject semiFinal2 = new GameObject(quarterFinal3, quarterFinal4);
-		
-		GameObject winner = game.tournament_winner(semiFinal1, semiFinal2);
-		
-		assertEquals("Richard", winner.getPlayer()[0]);
-		assertEquals("R", winner.getPlayer()[1]);
+		return result;
 	}
 }
