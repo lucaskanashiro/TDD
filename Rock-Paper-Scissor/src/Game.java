3fb949dc2a05d6999455537e73e842c36b82966e
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-05-21 13:12:43 -0300

    Passing test of create Game with players

--- Rock-Paper-Scissor/src/Game.java
+++ Rock-Paper-Scissor/src/Game.java
@@ -1,18 +1,19 @@
 
 public class Game {
 
+	private String[] player1, player2;
+	
 	public Game(String[] player1, String[] player2) {
-		// TODO Auto-generated constructor stub
+		this.player1 = player1;
+		this.player2 = player2;
 	}
 
 	public String[] getPlayer1() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.player1;
 	}
 
 	public String[] getPlayer2() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.player2;
 	}
 
 }
