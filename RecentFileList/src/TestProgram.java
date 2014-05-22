a3980d278ad30a39434551c5e78cd76a4dc9d09b
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-05-22 11:05:07 -0300

    Breaking test of more than maximum opened files
    In recentFileList maximum is 15

--- RecentFileList/src/TestProgram.java
+++ RecentFileList/src/TestProgram.java
@@ -75,4 +75,29 @@ public class TestProgram {
 		
 		assertEquals(shouldReturn, this.prog.getRecentFileList());
 	}
+	
+	@Test
+	public void testReturnOfRecentFileListWithMoreThanMaximumFilesOpenend() throws RecentFileNullException{
+		this.prog.openFile("TDD.txt");
+		this.prog.openFile("DAS.txt");
+		this.prog.openFile("A.txt");
+		this.prog.openFile("B.txt");
+		this.prog.openFile("C.txt");
+		this.prog.openFile("D.txt");
+		this.prog.openFile("E.txt");
+		this.prog.openFile("F.txt");
+		this.prog.openFile("G.txt");
+		this.prog.openFile("H.txt");
+		this.prog.openFile("I.txt");
+		this.prog.openFile("J.txt");
+		this.prog.openFile("K.txt");
+		this.prog.openFile("L.txt");
+		this.prog.openFile("M.txt");
+		this.prog.openFile("last.txt");
+		
+		String shouldReturn = "DAS.txt A.txt B.txt C.txt D.txt E.txt F.txt " +
+				"G.txt H.txt I.txt J.txt K.txt L.txt M.txt last.txt";
+		
+		assertEquals(shouldReturn, this.prog.getRecentFileList());
+	}
 }
